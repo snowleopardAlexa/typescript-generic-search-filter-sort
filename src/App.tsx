612c5403ffcff301ae3b4d2f7c1b667931f69d3c
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-17 21:31:28 -0400

    import widgets and people mock data, map over it in app.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import './App.css';
+import people from './mock-data/people';
+import widgets from './mock-data/widgets';
 
 function App() {
   return (
     <div className="App">
-      
+      <h2>Widgets:</h2>
+      {widgets.map(widget => {
+        return (
+          <h3>{widget.title}</h3>
+        )
+      })}
+      <h2>People:</h2>
+      {people.map(person => {
+        return (
+          <h3>{person.firstName} {person.lastName}</h3>
+        )
+      })}
     </div>
   );
 }
