9d986d596c17733b2888fcb5787c89797c053760
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-17 21:03:55 -0400

    add flexible and resuable sort function

--- src/generics/generics.ts
+++ src/generics/generics.ts
@@ -1,48 +1,73 @@
-interafce IVeganRestaurant {
-    vegan: string
-    restaurant: string
-}
-
-const veganRestaurants: Array<IVeganRestaurant> = [
-    {
-      vegan: "vegan1",
-      restaurant: "restaurant1"
-    },
-    {
-      vegan: "I am vegan two",
-      restaurant: "I am restaurant two"
-    },
-    {
-      vegan: "I am vegan three",
-      restaurant: "I am restaurant three"
-    }
-]
-
-function sortByVegan(veganRestaurants: Array<IVeganRestaurant>) {
-    veganRestaurants.sort((a, b) => {
-        if (a.vegan > b.vegan) {
-            return 1
-        }
-        if (a.vegan < b.vegan) {
-            return -1
-        }
-        return 0
-    })
-}
-
-function sortByRestaurant(veganRestaurants: Array<IVeganRestaurnt>) {
-    veganRestaurants.sort((a, b) => {
-        if (a.vegan > b.vegan) {
-            return 1
-        }
-        if (a.vegan < b.vegan) {
-            return -1
-        }
-        return 0
-    })
-}
-
 // generics 
 // start with <> and capital letter T - sometimes it starts with T but you might see letters like: T, U, V
 // generics types: T, U, V
-// 
+// different conventions: S - state, P -props, SS - snapshot - generic types 
+// keyof 
+interface IFooBar {
+    foo: string;
+    bar: string;
+}
+
+const fooBars: Array<IFooBar> = [
+    {
+        foo: "foo1",
+        bar: "bar1"
+    },
+    {
+        foo: "i am foo two",
+        bar: "i am bar two"
+    },
+    {
+        foo: "foo three",
+        bar: "bar three"
+    }
+]
+
+// we don't need it
+function sortByFoo(fooBars: Array<IFooBar>) {
+    fooBars.sort((a, b) => {
+        if (a.foo > b.foo) {
+            return 1;
+        }
+        if (a.foo < b.foo) {
+            return -1;
+        }
+        return 0;
+    })
+}
+
+// we don't need it 
+function sortByBar(fooBars: Array<IFooBar>) {
+    fooBars.sort((a, b) => {
+        if (a.bar > b.bar) {
+            return 1;
+        }
+        if (a.bar < b.bar) {
+            return -1;
+        }
+        return 0;
+    })
+}
+
+// we don't need to write multiple functions for every interface type: foo, bar, cat
+// we can only write generic function for all data - it is more efficient
+// flexible sort function
+// flexible and reusable code
+function sortByKey<T>(data: Array<T>, key: keyof T) {
+    data.sort((a, b) => {
+        if (a[key] > b[key]) {
+            return 1;
+        }
+        if (a[key] < b[key]) {
+            return -1;
+        }
+        return 0;
+    })
+}
+
+// Both fine: foo and bar are properties of IFooBar!
+sortByKey<IFooBar>(fooBars, "foo")
+sortByKey<IFooBar>(fooBars, "bar")
+
+// TypeScript complains: cat is not a property of IFooBar!
+sortByKey<IFooBar>(fooBars, "cat")
