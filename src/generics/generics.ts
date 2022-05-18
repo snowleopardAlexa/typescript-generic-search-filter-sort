51c7029a1cc5bbc5d1b7c74a89e37260cceb5146
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-17 21:18:57 -0400

    add class

--- src/generics/generics.ts
+++ src/generics/generics.ts
@@ -71,3 +71,56 @@ sortByKey<IFooBar>(fooBars, "bar")
 
 // TypeScript complains: cat is not a property of IFooBar!
 sortByKey<IFooBar>(fooBars, "cat")
+
+
+////////////////// 2 ///////////////////
+
+// main class
+class Animal {
+    public legCount: number
+    constructor(legCount: number) {
+        this.legCount = legCount
+    }
+}
+
+// extenstion of a class
+class Cat extends Animal {
+    constructor() {
+        super(4)
+    }
+}
+
+// extension of a class
+class Monkey extends Animal {
+    constructor() {
+        super(2)
+    }
+}
+
+// extension of a class
+class Bacteria {
+
+}
+
+// we want to print out the legs of cat and monkey 
+// we want to print out only one function that can print the legs of class Animal
+// use generics to do it 
+
+// T is not good enough
+// use extends keyword
+// TS will accept any type as long as any time extends Animal
+function printLegCount<T extends Animal>(animal: T) {
+    console.log(`My leg count is: ${animal.legCount}`)
+}
+
+const myCat = new Cat()
+const myMonkey = new Monkey()
+
+printLegCount(myCat)
+printLegCount(myMonkey)
+
+
+const myBacteria = new Bacteria()
+// Argument of type 'Bacteria' is not assignable to parameter of type 'Animal'.
+// Why? --> because class Bacteria does not extend Animal
+printLegCount(myBacteria)
