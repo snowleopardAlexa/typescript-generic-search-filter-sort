39db741feb9ef8e1458e3667bfa23e90df2d89ce
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-21 12:53:23 -0400

    add Sorters.tsx comp

--- src/components/Sorters.tsx
+++ src/components/Sorters.tsx
@@ -1,18 +1,22 @@
 export interface ISortersProps {
-
+   object: T
 }
 
-export function Sorters (props: ISortersProps) {
+export function Sorters<T>(props: ISortersProps<T>) {
+    const { object } = props
     return (
         <>
           <label htmlFor="sorters" className="mt-3">Sorters! Try us too</label>
           <select
            id="sorters"
-           className="custom-select"
-          >
-          <option key={} value={}>
-              Some label here: 
-          </option>
+           className="custom-select">
+           {Object.keys(object).map((key) => {
+               return (
+                <option key={key} value={key}>
+                  Sort by '{key}'
+                </option>
+               )
+           })}
           </select>
         </>
     )
