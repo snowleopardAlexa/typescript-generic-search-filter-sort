7c96c28c7f712646e2cab4b68be7a5223a5b6c96
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-21 13:19:30 -0400

    add moment package

--- src/components/renderers/WidgetRenderer.tsx
+++ src/components/renderers/WidgetRenderer.tsx
@@ -1,7 +1,8 @@
+import Moment from "react-moment";
 import IWidget from "../../interfaces/IWidget";
 
 export function WidgetRenderer (props: IWidget) {
-    const { isSpecialCard, title, description, rating } = props
+    const { isSpecialCard, title, description, rating, created, updated, id } = props
     return (
         <div className="col-12 p-3">
           <div className={isSpecialCard ? "card specialCard" : "card"}>
@@ -9,6 +10,11 @@ export function WidgetRenderer (props: IWidget) {
                   <h1 className="card-title">{title}</h1>
                   <p className="card-text">{description}</p>
                   <p className="card-text font-italic">Rating: {rating}/10</p>
+              </div>
+              <div className="card-footer text-muted text-right">
+                  <span className="float-left">#{id}</span> created:
+                  <Moment fromNow date={created} />
+                  updated: <Moment fromNow date={updated} />
               </div>
           </div>
         </div>
